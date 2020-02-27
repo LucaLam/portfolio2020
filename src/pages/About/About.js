f2566299cc888ec38f1b66e53015f3c8e45e1bfe
Luca Lamanna <luca.lamanna@hotmail.com>
2020-02-26 23:23:49 -0500

    added basic styles to about me page and created contact page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,14 +4,33 @@ import './about.scss'
 
 const About =()=> {
         return (
-            <div>
-                <p>After 2+ years of teaching English in Quito, Ecuador, I was on the pursuit for something that offered regular challenges and greater opportunities to learn. That's when I found coding.</p>
-                <p>I’ve recently graduated from BrainStation’s 12-week full-time intensive Web Development program and during my time at BrainStation, I have not only learned many programming languages and frameworks, but I’ve also learned to embrace the challenges that development presents.</p>
-                <p>I'm hoping to find an opportunity to code meaningful technology with cool people and I hope that technology will better the lives of many.</p>
-                <br/>
-                <p>When I'm not working or coding, you can find me on a hike and snapping photos, doing brazilian jiu-jitsu, or making a cocktail with Campari.</p>
-            </div>
-        )
+          <div className="about-container">
+            <p className="about-text">
+              After 2+ years of teaching English in Quito, Ecuador, I was on the
+              pursuit for something that offered regular challenges and greater
+              opportunities to learn. That's when I found coding.
+            </p>
+            <br/>
+            <p className="about-text">
+              I’ve recently graduated from BrainStation’s 12-week full-time
+              intensive Web Development program and during my time at
+              BrainStation, I have not only learned many programming languages
+              and frameworks, but I’ve also learned to embrace the challenges
+              that development presents.
+            </p>
+            <p className="about-text">
+              I'm hoping to find an opportunity to code meaningful technology
+              with cool people and I hope that technology will better the lives
+              of many.
+            </p>
+            <br />
+            <p className="about-text">
+              When I'm not working or coding, you can find me on a hike and
+              snapping photos, doing brazilian jiu-jitsu, or making a cocktail
+              with Campari.
+            </p>
+          </div>
+        );
 }
 
 export default About
