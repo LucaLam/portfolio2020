6fa2c3332753f31894231739518172151125cd6e
Luca Lamanna <luca.lamanna@hotmail.com>
2020-03-02 23:00:34 -0500

    button now conditionally renders its styled components

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -12,10 +12,10 @@ const Card =({project})=> {
             <p className='project-card-description'>{description}</p>
             <div className='button-container'>
             <a href={gitUrl} target='_blank' rel="noopener noreferrer">
-            <div className='button'>See Code</div>
+            <div className={!gitUrl ? 'blank-out' : 'button' }>See Code</div>
             </a>
             <a href={projectUrl} target='_blank' rel="noopener noreferrer">
-            <div className='button'>Online</div>
+            <div className={!projectUrl ? 'blank-out' : 'button' }>Online</div>
             </a>
             </div>
             </div>
