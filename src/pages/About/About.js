d5faeedd96fd0926bef772a3ecabbbf22cdf3d66
Luca Lamanna <luca.lamanna@hotmail.com>
2020-03-25 14:22:08 -0400

    style changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,6 +15,7 @@ const About =()=> {
               pursuit for something that offered regular challenges and greater
               opportunities to learn. That's when I found coding.
             <br />
+            <br />
             
               I’ve recently graduated from BrainStation’s full-time
               intensive Web Development program and during my time at
@@ -22,9 +23,11 @@ const About =()=> {
               and frameworks, but I’ve also learned to embrace the challenges
               that development presents.
               <br />
+              <br />
               I'm hoping to find an opportunity to code meaningful technology
               with cool people and I hope that technology will better the lives
               of many.
+              <br />
               <br />
               When I'm not working or coding, you can find me on a hike and
               snapping photos, doing Brazilian Jiu-Jitsu, or making a cocktail
